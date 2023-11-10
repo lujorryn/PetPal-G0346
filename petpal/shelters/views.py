e8a3b60c22e27690312e7363b75579c9991ab4fc
jfongli <113391378+jfongli@users.noreply.github.com>
2023-11-10 14:41:40 -0500

    Added pagination to list views + permission decorator
    Still needs testing

--- petpal/shelters/views.py
+++ petpal/shelters/views.py
@@ -1,10 +1,12 @@
 # from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 from accounts.models import PetPalUser, Shelter
 from .serializers import ShelterSerializer
-
+from django.core.paginator import Paginator
 
 from django.http import JsonResponse
 
@@ -16,26 +18,61 @@ ENDPOINT: /api/shelters
 METHOD: GET
 PERMISSION:
 '''
-@api_view(['GET'])
-def shelters_list_view(request):
-    # Check that this is a logged-in user
-    if request.user.is_authenticated:
-        shelters = Shelter.objects.all()
-        serializer = ShelterSerializer(shelters, many=True)
-        return Response({'msg': 'List of shelters', 'data': serializer.data}, status=status.HTTP_200_OK)
 
-    return Response({'error': 'User is not logged in'}, status=status.HTTP_403_FORBIDDEN)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def shelters_list_view(request):
+    shelters = Shelter.shelter.all()
+    # serializer = ShelterSerializer(shelters, many=True)
+    data = []
+
+    # -- Pagination --
+    # Picks how many shelters to show per page
+    paginator = Paginator(shelters, per_page=3)
+    # Retrieve page number
+    page_num = request.GET.get("page", 1)
+    # Get shelters from that page number
+    page_obj = paginator.get_page(page_num)
+
+    # Append each shelter of the page in data lst
+    for shelter in page_obj.object_list:
+        data.append({
+            'id': shelter.pk,
+            'email': shelter.email,
+            'address': shelter.address,
+            'city': shelter.city,
+            'province': shelter.province,
+            'postal_code': shelter.postal_code,
+            'phone': shelter.phone,
+            'avatar': shelter.avatar,
+            'description': shelter.description
+        })
+
+    payload = {
+        "page": {
+            "current": page_obj.number,
+            "has_next": page_obj.has_next(),
+            "has_previous": page_obj.has_previous(),
+        },
+        "data": data
+    }
+
+    return Response(payload, status=status.HTTP_200_OK)
+
 
 '''
 VIEW / EDIT / DELETE A shelter
-ENDPOINT: /api/shelters/<str:account_id>/
+ENDPOINT: /api/shelters/<int:account_id>/
 METHOD: GET, PUT, DELETE
 PERMISSION:
 SUCCESS:
 '''
-@api_view(['GET', 'PUT'])
-def shelter_detail_view(request, account_id):
 
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def shelter_detail_view(request, account_id):
     # Check that the shelter exists
     try:
         shelter = Shelter.objects.get(id=account_id)
@@ -44,14 +81,9 @@ def shelter_detail_view(request, account_id):
 
     # METHODS
     if request.method == 'GET':
-        # Check that this is a logged-in user
-        if request.user.is_authenticated:
-            # Give details of the shelter
-            serializer = ShelterSerializer(shelter, many=False)
-            return Response({'msg': 'Shelter Detail', 'data': serializer.data}, status=status.HTTP_200_OK)
-
-        else:
-            return Response({'error': 'User is not logged in'}, status=status.HTTP_403_FORBIDDEN)
+        # Give details of the shelter
+        serializer = ShelterSerializer(shelter, many=False)
+        return Response({'msg': 'Shelter Detail', 'data': serializer.data}, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
         # Check that the request is from the shelter themselves
@@ -71,5 +103,3 @@ def shelter_detail_view(request, account_id):
             return Response({'msg': 'edit shelter', 'data': serialized.data}, status=status.HTTP_200_OK)
 
         return Response({'error': 'Unauthorized'}, status=status.HTTP_403_FORBIDDEN)
-
-
