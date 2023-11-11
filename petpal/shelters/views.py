fdce746a96a7035db468a944a1ec756902601243
jfongli <113391378+jfongli@users.noreply.github.com>
2023-11-10 23:57:21 -0500

    Added error message for invalid forms

--- petpal/shelters/views.py
+++ petpal/shelters/views.py
@@ -96,5 +96,7 @@ def shelter_detail_view(request, account_id):
             if serializer.is_valid():
                 serializer.save()
                 return Response({'msg': 'Shelter Updated', 'data': serializer.data}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'error': 'Unauthorized'}, status=status.HTTP_403_FORBIDDEN)
