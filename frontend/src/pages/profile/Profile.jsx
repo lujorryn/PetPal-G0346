bae3bb58f3db8181ced5582910d71801e8d1e537
Yanbo Cheng <86802756+iamyanbo@users.noreply.github.com>
2023-11-29 12:24:03 -0500

    Profile implementation

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -2,106 +2,267 @@ import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/AuthContext'
 import './Profile.css'
 import '../../styles/tailwind.css'
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
   const { token, userId, role } = useAuth()
   const [user, setUser] = useState(null)
+  const [shelterPets, setShelterPets] = useState(null)
+  const [favPets, setFavPets] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
+    if (!token) return navigate('/login')
     // Fetch user data
-    if (role === 'seeker') {
-      fetch(`${process.env.REACT_APP_API_URL}/api/seekers/${userId}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`
+    fetch(`${process.env.REACT_APP_API_URL}/api/${role}s/${userId}`, { // TODO: Change to /api/seekers/${userId
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => res.json())
+      .then(data => setUser(data))
+      .catch(err => console.log(err))
+    
+    // Different fetches for different roles
+    const fetchUserData = async () => {
+      try {
+        if (role === 'shelter') {
+          const data = await getShelterPets();
+          setShelterPets(data);
+        } else if (role === 'seeker') {
+          const data = await getFavPets();
+          setFavPets(data);
         }
-      }).then(res => res.json())
-        .then(data => setUser(data))
-        .catch(err => console.log(err))
-    } else {
-      fetch(`${process.env.REACT_APP_API_URL}/api/shelters/${userId}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }).then(res => res.json())
-        .then(data => setUser(data))
-        .catch(err => console.log(err))
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }, [token, userId, role])
 
-  return (
-    <div class="profile-grid">
-      <div class="profile-img">
-          <div class="profile-container">
-            <img class="rounded-full w-full h-full" src={`${process.env.REACT_APP_API_URL}${user?.user_data.avatar}`} alt="profile" />
-          </div>
-          <a href="profile/edit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Edit Profile</a>
-      </div>
-      <div class="profile-desc">
-        <p class="font-semibold text-5xl break-all">Your Profile</p>
-        <hr class="line"></hr>
-        <ul class="profile-list">
-          <li>
-            <span class="font-bold">Email:</span>
-            <span class="text-gray-700">
-              <a href="mailto: {user?.user_data.email}">{user?.user_data.email}</a>
-            </span>
-          </li>
-          <li>
-            <span class="font-bold">Adress:</span>
-            <span class="text-gray-700">
-              <p>{user?.user_data.address === '' ? 'N/A' : user?.user_data.address}</p>
-            </span>
-          </li>
-          <li>
-            <span class="font-bold">Postal Code:</span>
-            <span class="text-gray-700">
-              <p>{user?.user_data.postal_code === '' ? 'N/A' : user?.user_data.postal_code}</p>
-            </span>
-          </li>
-          <li>
-            <span class="font-bold">City:</span>
-            <span class="text-gray-700">
-              <p>{user?.user_data.city === '' ? 'N/A' : user?.user_data.city}</p>
-            </span>
-          </li>
-          <li>
-            <span class="font-bold">Province:</span>
-            <span class="text-gray-700">
-              <p>{user?.user_data.province === '' ? 'N/A' : user?.user_data.province}</p>
-            </span>
-          </li>
-          <li>
-            <span class="font-bold">Phone:</span>
-            <span class="text-gray-700">
-              <p>{user?.user_data.phone === '' ? 'N/A' : user?.user_data.phone}</p>
-            </span>
-          </li>
-          <li>
-            <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-3 rounded-full" href="applications">My Applications</a>
-          </li>
-        </ul>
-      </div>
-      <div class="profile-favs">
-        <div class="container mx-auto mb-4 px-5 py-2 lg:px-32 lg:pt-12 space-y-4">
-          <p class="text-xl font-bold">Your Favorites</p>
-          <div class="-m-1 flex flex-wrap md:-m-2">
-            {user?.fav_pets.map((pet) => (
-              <div class="flex w-full md:w-1/2 lg:w-1/3 p-1 md:p-2 flex justify-center items-center">
-                <a href={`/petlistings/${pet.id}`}>
-                  <img
-                  alt="gallery"
-                  class="block h-64 w-full rounded-lg object-cover object-center"
-                  src="TODO: Add image here"
-                  />
-                </a>
+    const getShelterPets = async () => {
+      let nextPage = `${process.env.REACT_APP_API_URL}/api/petlistings/shelter/${user?.data.email}`;
+      const allResults = [];
+    
+      try {
+        while (nextPage) {
+          const response = await fetch(nextPage, {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+    
+          const data = await response.json();
+          allResults.push(...data.results.data);
+          nextPage = data.next;
+        }
+        return allResults;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    const getFavPets = async () => {
+      let nextPage = `${process.env.REACT_APP_API_URL}/api/seekers/${userId}/favorites`;
+      const allResults = [];
+      var pageNum = 1;
+      try {
+        while (nextPage) {
+          const response = await fetch(nextPage, {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+
+          const data = await response.json();
+          allResults.push(...data.data);
+          if (data.page.has_next) {
+            nextPage = `${process.env.REACT_APP_API_URL}/api/seekers/${userId}/favorites?page=${pageNum + 1}`;
+            pageNum += 1;
+          } else {
+            break;
+          }
+        }
+        return allResults;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUserData();
+  }, [token, userId, role, navigate, user?.data.email])
+
+
+
+  const renderProfile = () => {
+    if (role === 'seeker') {
+      return (
+        <div class="profile-grid">
+          <div class="profile-img">
+              <div class="profile-container">
+                <img class="rounded-full w-full h-full" src={user?.data.avatar? `${process.env.REACT_APP_API_URL}${user?.data.avatar}` : "../../../images/logo_ref.png"} alt="../../../images/logo_ref.png" />
               </div>
-            ))}
+              <a href="profile/edit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Edit Profile</a>
+          </div>
+          <div class="profile-desc">
+            <p class="font-semibold text-5xl break-all">Your Profile</p>
+            <hr class="line"></hr>
+            <ul class="profile-list">
+              <li>
+                <span class="font-bold">Email:</span>
+                <span class="text-gray-700">
+                  <a href="mailto: {user?.data.email}">{user?.data.email}</a>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Adress:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.address === '' ? 'N/A' : user?.data.address}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Postal Code:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.postal_code === '' ? 'N/A' : user?.data.postal_code}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">City:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.city === '' ? 'N/A' : user?.data.city}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Province:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.province === '' ? 'N/A' : user?.data.province}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Phone:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.phone === '' ? 'N/A' : user?.data.phone}</p>
+                </span>
+              </li>
+              <li>
+                <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-3 rounded-full" href="applications">My Applications</a>
+              </li>
+            </ul>
+          </div>
+          <div class="profile-favs">
+            <div className="container mx-auto mb-4 px-5 py-2 lg:px-32 lg:pt-12 space-y-4">
+              <p className="text-xl font-bold">Your Favorites</p>
+              <div className="-m-1 flex flex-wrap md:-m-2">
+                {favPets?.map((pet) => (
+                  <div className="flex w-full md:w-1/2 lg:w-1/3 p-1 md:p-2">
+                    <a href={`/petlistings/${pet.id}`} className="w-full block">
+                      <img
+                        alt={pet.name}
+                        className="block w-full h-64 object-cover object-center rounded-lg"
+                        src={pet.photos[0] ? `${process.env.REACT_APP_API_URL}${pet.photos[0].url}` : "../../../images/logo_ref.png"}
+                      />
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )
+    } else {
+      return (
+        <div class="profile-grid">
+          <div class="profile-img">
+              <div class="profile-container">
+                <img class="rounded-full w-full h-full" src={user?.data.avatar? `${process.env.REACT_APP_API_URL}${user?.data.avatar}` : "../../../images/logo_ref.png"} alt="../../../images/logo_ref.png" />
+              </div>
+              <a href="profile/edit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Edit Profile</a>
+          </div>
+          <div class="profile-desc">
+            <p class="font-semibold text-5xl break-all">Your Profile</p>
+            <hr class="line"></hr>
+            <ul class="profile-list">
+              <li>
+                <span class="font-bold">Email:</span>
+                <span class="text-gray-700">
+                  <a href="mailto: {user?.data.email}">{user?.data.email}</a>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Adress:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.address === '' ? 'N/A' : user?.data.address}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Postal Code:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.postal_code === '' ? 'N/A' : user?.data.postal_code}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">City:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.city === '' ? 'N/A' : user?.data.city}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Province:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.province === '' ? 'N/A' : user?.data.province}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Phone:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.phone === '' ? 'N/A' : user?.data.phone}</p>
+                </span>
+              </li>
+              <li>
+                <span class="font-bold">Mission Statement:</span>
+                <span class="text-gray-700">
+                  <p>{user?.data.description === '' ? 'N/A' : user?.data.description}</p>
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div class="profile-favs">
+            <div class="container mx-auto mb-4 px-5 py-2 lg:px-32 lg:pt-12 space-y-4">
+              <div class="shelter-pet-list">
+                <p class="text-xl font-bold center-text mr-4">Listed Pets</p>
+                  <a href="petlistings/create">
+                    <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-4 rounded-full">
+                      +
+                    </button>
+                  </a>
+              </div>
+              <div className="-m-1 flex flex-wrap md:-m-2">
+                {shelterPets?.map((pet) => (
+                  <div className="flex w-full md:w-1/2 lg:w-1/3 p-1 md:p-2 flex flex-col items-center">
+                    <a href={`/petlistings/${pet.id}/edit`} className="w-full block">
+                      <img
+                        alt={pet.name}
+                        className="block w-full h-64 rounded-lg object-cover object-center"
+                        src={pet.photos[0] ? `${process.env.REACT_APP_API_URL}${pet.photos[0].url}` : "../../../images/logo_ref.png"}
+                      />
+                    </a>
+                    <p className="text-center">{pet.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div class="w-full h-full">
+      {user && renderProfile()}
     </div>
+
+
   )
 }
 
--- frontend/src/pages/profile/ProfileEdit.jsx
+++ frontend/src/pages/profile/ProfileEdit.jsx
@@ -32,7 +32,9 @@ function ProfileEdit() {
     }
     formData.append('is_notif_comment', document.getElementById('comment-checkbox').checked)
     formData.append('is_notif_status', document.getElementById('status-checkbox').checked)
-    formData.append('is_notif_petlisting', document.getElementById('petlisting-checkbox').checked)
+    if (role === 'seeker') {
+      formData.append('is_notif_petlisting', document.getElementById('petlisting-checkbox').checked)
+    }
     fetch(`${process.env.REACT_APP_API_URL}/api/${role}s/${userId}`, {
       method: 'PUT',
       headers: {
@@ -44,67 +46,140 @@ function ProfileEdit() {
       .catch(err => console.log(err))
   }
 
+  const renderProfile = () => {
+    if (role === 'seeker') {
+      return (
+        <div class="profile-grid">
+          <div class="profile-img">
+            <div class="profile-container">
+              <img class="rounded-full w-full h-full" src={user?.data.avatar? `${process.env.REACT_APP_API_URL}${user?.data.avatar}` : "../../../images/logo_ref.png"} alt="../../../images/logo_ref.png" />
+              </div>
+            <input type="file" id="profile-image" name="profile-image" accept="image/*"
+              onChange={(e) => setAvatar(e.target.files[0])}
+            />
+          </div>
+          <div class="profile-desc">
+            <p class="font-semibold text-5xl break-all">Your Profile</p>
+            <hr class="line"></hr>
+            <form class="profile-form" id="profile-form" onSubmit={handleSubmit}>
+              <div class="mb-4">
+                <label for="email" class="font-bold">Email:</label>
+                <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.email} required/>
+              </div>
+              <div class="mb-4">
+                <label for="address" class="font-bold">Address:</label>
+                <input type="text" id="address" name="address" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.address}/>
+              </div>
+              <div class="mb-4">
+                <label for="postal" class="font-bold">Postal Code:</label>
+                <input type="text" id="postal" name="postal" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.postal_code}/>
+              </div>
+              <div class="mb-4">
+                <label for="city" class="font-bold">City:</label>
+                <input type="text" id="city" name="city" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.city}/>
+              </div>
+              <div class="mb-4">
+                <label for="province" class="font-bold">Province:</label>
+                <input type="text" id="province" name="province" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.province}/>
+              </div>
+              <div class="mb-4">
+                <label for="phone" class="font-bold">Phone:</label>
+                <input type="tel" id="phone" name="phone" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.phone}/>
+              </div>
+              <div class="flex items-center mb-4">
+                <input id="comment-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  defaultChecked={user?.data.is_notif_comment}
+                />
+                <label for="comment-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for comments</label>
+              </div>
+              <div class="flex items-center mb-4">
+                <input id="status-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  defaultChecked={user?.data.is_notif_status}
+                />
+                <label for="status-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for status change</label>
+              </div>
+              <div class="flex items-center mb-4">
+                <input id="petlisting-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  defaultChecked={user?.data.is_notif_petlisting}
+                />
+                <label for="petlisting-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for petlistings</label>
+              </div>
+              <div class="mb-4">
+              <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-3 rounded-full">Update Profile</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div class="profile-grid">
+          <div class="profile-img">
+            <div class="profile-container">
+              <img class="rounded-full w-full h-full" src={user?.data.avatar? `${process.env.REACT_APP_API_URL}${user?.data.avatar}` : "../../../images/logo_ref.png"} alt="../../../images/logo_ref.png" />
+              </div>
+            <input type="file" id="profile-image" name="profile-image" accept="image/*"
+              onChange={(e) => setAvatar(e.target.files[0])}
+            />
+          </div>
+          <div class="profile-desc">
+            <p class="font-semibold text-5xl break-all">Your Profile</p>
+            <hr class="line"></hr>
+            <form class="profile-form" id="profile-form" onSubmit={handleSubmit}>
+              <div class="mb-4">
+                <label for="email" class="font-bold">Email:</label>
+                <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.email} required/>
+              </div>
+              <div class="mb-4">
+                <label for="address" class="font-bold">Address:</label>
+                <input type="text" id="address" name="address" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.address}/>
+              </div>
+              <div class="mb-4">
+                <label for="postal" class="font-bold">Postal Code:</label>
+                <input type="text" id="postal" name="postal" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.postal_code}/>
+              </div>
+              <div class="mb-4">
+                <label for="city" class="font-bold">City:</label>
+                <input type="text" id="city" name="city" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.city}/>
+              </div>
+              <div class="mb-4">
+                <label for="province" class="font-bold">Province:</label>
+                <input type="text" id="province" name="province" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.province}/>
+              </div>
+              <div class="mb-4">
+                <label for="phone" class="font-bold">Phone:</label>
+                <input type="tel" id="phone" name="phone" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.phone}/>
+              </div>
+              <div class="mb-4">
+                <label for="description" class="font-bold">Mission Statement:</label>
+                <input type="tel" id="description" name="description" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.data.description}/>
+              </div>
+              <div class="flex items-center mb-4">
+                <input id="comment-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  defaultChecked={user?.data.is_notif_comment}
+                />
+                <label for="comment-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for comments</label>
+              </div>
+              <div class="flex items-center mb-4">
+                <input id="status-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  defaultChecked={user?.data.is_notif_status}
+                />
+                <label for="status-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for status change</label>
+              </div>
+              <div class="mb-4">
+              <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-3 rounded-full">Update Profile</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )
+    }
+  }
+
+
   return (
-    <div class="profile-grid">
-      <div class="profile-img">
-          <div class="profile-container">
-            <img class="rounded-full w-full h-full" src={`${process.env.REACT_APP_API_URL}${user?.user_data.avatar }`} alt="profile" />
-          </div>
-          <input type="file" id="profile-image" name="profile-image" accept="image/*"
-            onChange={(e) => setAvatar(e.target.files[0])}
-          />
-      </div>
-      <div class="profile-desc">
-        <p class="font-semibold text-5xl break-all">Your Profile</p>
-        <hr class="line"></hr>
-        <form class="profile-form" id="profile-form" onSubmit={handleSubmit}>
-          <div class="mb-4">
-            <label for="email" class="font-bold">Email:</label>
-            <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.user_data.email} required/>
-          </div>
-          <div class="mb-4">
-            <label for="address" class="font-bold">Address:</label>
-            <input type="text" id="address" name="address" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.user_data.address}/>
-          </div>
-          <div class="mb-4">
-            <label for="postal" class="font-bold">Postal Code:</label>
-            <input type="text" id="postal" name="postal" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.user_data.postal_code}/>
-          </div>
-          <div class="mb-4">
-            <label for="city" class="font-bold">City:</label>
-            <input type="text" id="city" name="city" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.user_data.city}/>
-          </div>
-          <div class="mb-4">
-            <label for="province" class="font-bold">Province:</label>
-            <input type="text" id="province" name="province" class="w-full px-3 py-2 border rounded-lg" defaultValue={user?.user_data.province}/>
-          </div>
-          <div class="mb-4">
-            <label for="phone" class="font-bold">Phone:</label>
-            <input type="tel" id="phone" name="phone" class="w-full px-3 py-2 border rounded-lg" defaultValue="123-456-7890"/>
-          </div>
-          <div class="flex items-center mb-4">
-            <input id="comment-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              defaultChecked={user?.user_data.is_notif_comment}
-            />
-            <label for="comment-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for comments</label>
-          </div>
-          <div class="flex items-center mb-4">
-            <input id="status-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              defaultChecked={user?.user_data.is_notif_status}
-            />
-            <label for="status-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for status change</label>
-          </div>
-          <div class="flex items-center mb-4">
-            <input id="petlisting-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              defaultChecked={user?.user_data.is_notif_petlisting}
-            />
-            <label for="petlisting-checkbox" class="ms-2 text-sm font-medium">Recieve Notifications for petlistings</label>
-          </div>
-          <div class="mb-4">
-          <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-3 rounded-full">Update Profile</button>
-          </div>
-        </form>
-      </div>
+    <div class="w-full h-full">
+      {renderProfile()}
     </div>
   )
 }
