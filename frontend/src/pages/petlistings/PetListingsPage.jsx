d95c0a9aebd59e0dc25539b3abed9a0aacbfa6e2
Jorryn Lu <jorryn.lu@mail.utoronto.ca>
2023-12-06 07:59:06 -0500

    Implement Applications List Page
    - Sort included
    - Search supports:
      - Seeker email / Shelter email
      - Seeker name
      - Petlisting name, category, size, gender
    - Pagination included (currently set at 5 per page)
    - List entries support withdraw for seekers, approve and decline for shelters
    - View button leads to each application respectively
    - Modify how the search query param in the url is handled

--- frontend/src/pages/petlistings/PetListingsPage.jsx
+++ frontend/src/pages/petlistings/PetListingsPage.jsx
@@ -46,18 +46,18 @@ function PetListings() {
       const parseSearchArr = searchTerm.split(' ')
       let name = ''
       for (let i in parseSearchArr) {
-        if(parseSearchArr[i] === 'dog') queryString = queryString + (queryString !== '' ? '&category=D' : 'category=D')
-        else if(parseSearchArr[i] === 'cat') queryString = queryString + (queryString !== '' ? '&category=C' : 'category=C')
-        else if(parseSearchArr[i] === 'other') queryString = queryString + (queryString !== '' ? '&category=O' : 'category=O')
-        else if(parseSearchArr[i] === 'small') queryString = queryString + (queryString !== '' ? '&size=S' : 'size=S')
-        else if(parseSearchArr[i] === 'medium') queryString = queryString + (queryString !== '' ? '&size=M' : 'size=M')
-        else if(parseSearchArr[i] === 'large') queryString = queryString + (queryString !== '' ? '&size=L' : 'size=L')
-        else if(parseSearchArr[i] === 'male') queryString = queryString + (queryString !== '' ? '&gender=M' : 'gender=M')
-        else if(parseSearchArr[i] === 'female') queryString = queryString + (queryString !== '' ? '&gender=L' : 'gender=L')
-        else if(parseSearchArr[i] === 'available') queryString = queryString + (queryString !== '' ? '&status=AV' : 'status=AV')
-        else if(parseSearchArr[i] === 'adopted') queryString = queryString + (queryString !== '' ? '&status=AD' : 'status=AD')
-        else if(parseSearchArr[i] === 'pending') queryString = queryString + (queryString !== '' ? '&status=PE' : 'status=PE')
-        else if(parseSearchArr[i] === 'withdrawn') queryString = queryString + (queryString !== '' ? '&status=WI' : 'status=WI')
+        if(parseSearchArr[i].toLowerCase() === 'dog') queryString = queryString + (queryString !== '' ? '&category=D' : 'category=D')
+        else if(parseSearchArr[i].toLowerCase() === 'cat') queryString = queryString + (queryString !== '' ? '&category=C' : 'category=C')
+        else if(parseSearchArr[i].toLowerCase() === 'other') queryString = queryString + (queryString !== '' ? '&category=O' : 'category=O')
+        else if(parseSearchArr[i].toLowerCase() === 'small') queryString = queryString + (queryString !== '' ? '&size=S' : 'size=S')
+        else if(parseSearchArr[i].toLowerCase() === 'medium') queryString = queryString + (queryString !== '' ? '&size=M' : 'size=M')
+        else if(parseSearchArr[i].toLowerCase() === 'large') queryString = queryString + (queryString !== '' ? '&size=L' : 'size=L')
+        else if(parseSearchArr[i].toLowerCase() === 'male') queryString = queryString + (queryString !== '' ? '&gender=M' : 'gender=M')
+        else if(parseSearchArr[i].toLowerCase() === 'female') queryString = queryString + (queryString !== '' ? '&gender=L' : 'gender=L')
+        else if(parseSearchArr[i].toLowerCase() === 'available') queryString = queryString + (queryString !== '' ? '&status=AV' : 'status=AV')
+        else if(parseSearchArr[i].toLowerCase() === 'adopted') queryString = queryString + (queryString !== '' ? '&status=AD' : 'status=AD')
+        else if(parseSearchArr[i].toLowerCase() === 'pending') queryString = queryString + (queryString !== '' ? '&status=PE' : 'status=PE')
+        else if(parseSearchArr[i].toLowerCase() === 'withdrawn') queryString = queryString + (queryString !== '' ? '&status=WI' : 'status=WI')
         else if(parseSearchArr[i].includes('@')) queryString = queryString + (queryString !== '' ? `&shelter_email=${parseSearchArr[i]}` : `shelter_email=${parseSearchArr[i]}`)
         else name = name + (name === '' ? parseSearchArr[i] : ("%20" + parseSearchArr[i]))
       }
@@ -78,7 +78,7 @@ function PetListings() {
 
   return (
     <PetlistingsPageWrapper>
-      <SearchBar searchTerm={searchTerm} />
+      <SearchBar setSearchTerm={setSearchTerm} />
       <PetlistingsSort setSortOption={setSortOption} setSortAsc={setSortAsc}/>
       <PetlistingsFilter setFilters={setFilters} />
       <PetlistingsDisplay data={data} setPage={setPage}/>
